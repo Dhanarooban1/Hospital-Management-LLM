22ce6097cf6af038e8944e80a713c3cf7f688017
Dhanarooban1 <dhanazx1@gmail.com>
2024-12-10 13:06:37 +0530

    added

--- Backend/Server.py
+++ Backend/Server.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 from Routers import user_bp
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*", "supports_credentials": True}})
 app.register_blueprint(user_bp, url_prefix='/')
+
 if __name__ == "__main__":
-    app.run(debug=True, host='localhost', port=5000)
+    # Get port from environment or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
