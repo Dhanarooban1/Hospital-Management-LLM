3b633a15f06ad3230f2fac3747d99692924104cf
Dhanarooban1 <dhanazx1@gmail.com>
2024-12-12 20:05:27 +0530

    Added backend link

--- Frontend/Vite-project/src/Functions/Controller.jsx
+++ Frontend/Vite-project/src/Functions/Controller.jsx
@@ -22,7 +22,7 @@ export const QueryProvider = ({ children }) => {
         setIsLoading(true);
 
         const response = await axios.post(
-          'http://127.0.0.1:5000/post/query',
+          'https://hospital-management-llm-backend.onrender.com/post/query',
           { question },
           {
             headers: {
